a44fef455a090c730ecbb0cb603b40b8beb2dc71
Hernan Martinez <hernanmartinezf@gmail.com>
2023-05-11 13:46:32 -0300

    corregidos nombres de imagenes, creando banner

---
+++ client/src/components/banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <img src="../images/bannerlogogrande.png" alt="CAR SHOP"></img>
+    </div>
+  )
+}
+
+export default Banner
