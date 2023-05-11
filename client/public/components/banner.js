05e106d46acca2bccb096d6b8f3621ddeb29e41f
Hernan Martinez <hernanmartinezf@gmail.com>
2023-05-11 13:09:39 -0300

    carga de imagenes y organizado de carpetas

---
+++ client/public/components/banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const banner = () => {
+  return (
+    <div>
+      <img src="carshop bannerlogo grande.png" alt="CAR SHOP"></img>
+    </div>
+  )
+}
+
+export default banner
