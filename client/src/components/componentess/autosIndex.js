14bd2e752b20647cbd2a888146f13a1f514ee4a0
gabriel <gabrielortino@yahoo.com.ar>
2023-06-23 20:58:58 -0300

    acomode pagina vistas

--- client/src/components/componentess/autosIndex.js
+++ client/src/components/componentess/autosIndex.js
@@ -1,25 +1,32 @@
 import React from 'react'
 
-
+import Banner from '../banner';
 import Descripcion from './descripcion';
 import Tres from './tres';
 import Dos from './dos';
 import Uno from './uno';
 import Contactos from './contactos';
+import Segurosyfinanciacion from './segurosyfinanciaciones';
 
+import '../../css/autosIndex.css';
 
 
 
 function autosIndex  ()  {
   return (
     <div class='autosIndex'>
+
+        <Banner/>
+
       <div class='prueba' >
         <Uno/>
+        <Dos/>
         <Tres/>
       </div>
 
       <div class='prueba2'>
         <Descripcion/>
+        <Segurosyfinanciacion/>
       </div>
 
       <div>
