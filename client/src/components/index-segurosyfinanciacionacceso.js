1d061c17a1f6f9b11695424bc83ca222f91ee88c
azariaspassarelli <azapassa@gmail.com>
2023-11-26 20:08:07 -0300

    modificaciones de estilos

--- client/src/components/index-segurosyfinanciacionacceso.js
+++ client/src/components/index-segurosyfinanciacionacceso.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import '../css/bloques.css';
-import creditoimg from '../images/credito1.jpg';
-import seguroimg from '../images/seguro1.jpg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../css/bloques.css";
+import creditoimg from "../images/credito1.jpg";
+import seguroimg from "../images/seguro1.jpg";
+import { Link } from "react-router-dom";
 
 const Segurosyfinanciacion = () => {
   return (
     <>
-      <div className='bloques'>
+      <div className="bloques">
         <div className="custom-row">
-          <div className="custom-col" >
+          <div className="custom-col">
             <div className="custom-card">
-            <Link to="/seguro">
-              <img
-                src={seguroimg}
-                className="custom-card-img-top"
-                alt="Asegura tu auto"
-                style={{ width: '300%', height: 'auto' }}               
-              />
-            </Link>
+              <Link to="/seguro">
+                <img
+                  src={seguroimg}
+                  className="custom-card-img-top"
+                  alt="Asegura tu auto"
+                  style={{ width: "230%", height: "auto" }}
+                />
+              </Link>
               <div className="custom-card-body">
                 <h5 className="custom-card-title">ASEGURÁ TU AUTO</h5>
               </div>
             </div>
           </div>
-          <div className="custom-col" >
+          <div className="custom-col">
             <div className="custom-card">
               <Link to="/credito">
                 <img
                   src={creditoimg}
                   className="custom-card-img-top"
                   alt="Simulá tu crédito"
-                  style={{ width: '250%', height: 'auto' }}
+                  style={{ width: "230%", height: "auto" }}
                 />
               </Link>
               <div className="custom-card-body">
@@ -43,6 +43,6 @@ const Segurosyfinanciacion = () => {
       </div>
     </>
   );
-}
+};
 
 export default Segurosyfinanciacion;
