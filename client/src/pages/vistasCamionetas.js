a126eb5bddbeac080268591e9f61409e8b26b3e3
Ramiro Quiñoneros <quinionerosramiro@gmail.com>
2023-11-27 18:16:11 -0300

    correciones en server

--- client/src/pages/vistasCamionetas.js
+++ client/src/pages/vistasCamionetas.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/autosIndex.css';
 
-import CamionetasIndex from '../components/componentess/camionetasIndex';
+import CamionetasIndex from '../components/componentess/CamionetasIndex';
 
 
 const vistasCamionetas = () => {
